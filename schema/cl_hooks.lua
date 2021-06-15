4712e5f2c7c3126d588248dd0f0688ce9ced2394
Alexandr Trifonov <mobious96@gmail.com>
2021-06-16 02:58:24 +0300

    bag and color modification

--- schema/cl_hooks.lua
+++ schema/cl_hooks.lua
@@ -6,15 +6,26 @@
 -- 	self:ExampleFunction("@serverWelcome", character:GetName())
 -- end
 
-
--- function Schema:ShouldShowPlayerOnScoreboard(client)
--- 	return false
--- end
-
 function Schema:BuildScoreboardMenu()
 	return LocalPlayer():IsAdmin()
 end
 
 function Schema:BuildBusinessMenu()
 	return false
+end
+
+local tab = {
+	[ "$pp_colour_addr" ] = 0,
+	[ "$pp_colour_addg" ] = 0,
+	[ "$pp_colour_addb" ] = 0,
+	[ "$pp_colour_brightness" ] = -0.01,
+	[ "$pp_colour_contrast" ] = 0.9,
+	[ "$pp_colour_colour" ] = 0.8,
+	[ "$pp_colour_mulr" ] = 0,
+	[ "$pp_colour_mulg" ] = 0,
+	[ "$pp_colour_mulb" ] = 0
+}
+
+function Schema:RenderScreenspaceEffects()
+	DrawColorModify(tab)
 end
---
+++ schema/items/bags/sh_bag1.lua
@@ -0,0 +1,15 @@
+ITEM.name = "Рюкзак"
+ITEM.description = "Обычный походный рюкзак"
+ITEM.model = "models/spec45as/stalker/items/rukzak1.mdl"
+ITEM.category = "Bags"
+ITEM.width = 2
+ITEM.height = 3
+ITEM.exRender = true
+ITEM.iconCam = {
+	pos = Vector(0, 0, 32.893634796143),
+	ang = Angle(90, 0, 0),
+	fov = 45,
+}
+
+ITEM.invWidth = 3
+ITEM.invHeight = 5
