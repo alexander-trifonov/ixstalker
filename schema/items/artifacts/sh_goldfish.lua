656f0a0250ddb40349ddc4f6489958c3cc818185
Alexandr Trifonov <mobious96@gmail.com>
2021-06-19 21:38:23 +0300

    artifacts

---
+++ schema/items/artifacts/sh_goldfish.lua
@@ -0,0 +1,6 @@
+ITEM.name = "Золотая Рыбка"
+ITEM.model = Model("models/spec45as/stalker/artefacts/af_gold_fish.mdl")
+ITEM.description = "Артефакт причудливой формы, которая и вправду похожа на золотую рыбку... или печень"
+
+ITEM.width = 1 -- Width and height refer to how many grid spaces this item takes up.
+ITEM.height = 1
---
+++ schema/items/base/sh_artifacts.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Artifacts Base"
+ITEM.model = "models/props_junk/garbage_metalcan002a.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.description = "Artifact"
+ITEM.category = "Artifacts"
+ITEM.bDropOnDeath = true
