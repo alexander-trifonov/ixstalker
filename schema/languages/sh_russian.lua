70d7215343f78e8b59616a54ca46df91eecf7ca7
Alexandr Trifonov <mobious96@gmail.com>
2021-06-23 15:07:17 +0300

    ** in me remove

--- schema/languages/sh_russian.lua
+++ schema/languages/sh_russian.lua
@@ -4,6 +4,6 @@ LANGUAGE = {
     rollFormat = "%s выбрасывает число %s",
     takeFormat = "%s поднимает %s",
     yFormat = "\n\tкричит: \"%s\"",
-    meFormat = "\n **%s**",
+    meFormat = "\n **%s",
     pmFormat = "\n[PDA]: %s"
 }
