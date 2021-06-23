ee29fc1adc6a3fd24537a2349b22ce26c381d45a
Alexandr Trifonov <mobious96@gmail.com>
2021-06-23 20:33:33 +0300

    ammo and weapons

---
+++ schema/items/weapons/sh_detector_echo.lua
@@ -0,0 +1,10 @@
+ITEM.name = "Детектор 'Эхо'"
+ITEM.description = "Прототип из научной лаборатории для обнаружения артефактов в Зоне"
+ITEM.class = "detector_echo"
+ITEM.weaponCategory = "secondary"
+ITEM.durability = 100
+ITEM.uniqueID = "detector_echo"
+
+ITEM.model = "models/spec45as/stalker/items/detector1.mdl"
+ITEM.width = 1
+ITEM.height = 1
---
+++ schema/items/weapons/sh_ump.lua
@@ -0,0 +1,22 @@
+ITEM.name = "UMP-45"
+ITEM.description = "Пистолет-пулимет высокой надежности был разработан к 1990-х годах немецкой компанией"
+ITEM.class = "arccw_mifl_fas2_mp5"
+ITEM.weaponCategory = "primary"
+ITEM.durability = 100
+ITEM.uniqueID = "ump45"
+
+ITEM.model = "models/weapons/arccw/mifl/fas2/c_mp5.mdl"
+ITEM.width = 3
+ITEM.height = 2
+
+ITEM.defaultAtt = {
+    [2] = "acwatt_mifl_fas2_mp5_ump_nor",
+    [8] = "acwatt_mifl_fas2_mp5_stock_ump"
+}
+
+ITEM.exRender = true
+ITEM.iconCam = {
+	pos = Vector(20.606443405151, 46.667194366455, -7.7285885810852),
+	ang = Angle(0, 270, 0),
+	fov = 45
+}
